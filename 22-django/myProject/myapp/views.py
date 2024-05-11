23dea71fe28b1f0644c729150fb402a7141c769e
Yifang-Guo <charlizeguo@gmail.com>
2024-05-11 16:39:14 +0100

    added user management in proj22

--- 22-django/myProject/myapp/views.py
+++ 22-django/myProject/myapp/views.py
@@ -1,32 +1,53 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our service is very fast.'
+    features = Feature.objects.all()
 
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Quality'
-    feature2.details = 'Our service has very high quality.'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Reputation'
-    feature3.details = 'Our service has high reputation.'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Service'
-    feature4.details = 'Our service is 24/7.'
-
-    return render(request, 'index.html', {'feature1' : feature1, 'feature2' : feature2, 'feature3' : feature3, 'feature4' : feature4})
+    return render(request, 'index.html', {'features' : features})
 
 def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+def register(request):
+    if request.method == 'POST':
+        userName = request.POST['userName']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(email= email).exists():
+            messages.info(request, 'Email already used.')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(username= userName,  email=email, password=password)
+            user.save()
+            return redirect('login')
+    else:
+        return render(request, 'register.html')
+    
+
+def login(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=name, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
